e81690af368e410d0d2dd510351c514cead83e0b
MusiliC <musilibrian07@gmail.com>
2023-10-27 21:53:34 +0300

    New implementation

--- src/main/java/com/cee/tech/App.java
+++ src/main/java/com/cee/tech/App.java
@@ -1,14 +1,10 @@
 package com.cee.tech;
 
 
-import com.cee.tech.oop.service.SportsClassService;
-
 public class App
 {
     public static void main( String[] args )
     {
-
-       SportsClassService app = new SportsClassService();
-       app.startApplication();
+      System.out.println("Hi");
     }
 }
--- src/main/java/com/cee/tech/oop/service/BallGamesService.java
+++
@@ -1,8 +0,0 @@
-package com.cee.tech.oop.service;
-
-public abstract class BallGamesService extends SportsClassService{
-    abstract void totalPlayers();
-    abstract void homeTeam();
-    abstract void awayTeam();
-    abstract void gameTime();
-}
